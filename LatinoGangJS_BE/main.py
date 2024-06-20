46c64f2b6f3e83f62f9e993b360df1cbc7368e9e
MoNKy0112 <jomoncaleanoa@unal.edu.co>
2024-06-20 14:00:32 -0500

    Backend endpoint ("\translate") created

--- LatinoGangJS_BE/main.py
+++ LatinoGangJS_BE/main.py
@@ -1,15 +1,38 @@
 import sys
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from antlr4 import *
 from grammar.gen.LatinoGrammarLexer import LatinoGrammarLexer
 from grammar.gen.LatinoGrammarParser import LatinoGrammarParser
 from LatinoToJs import LatinoToJs
+import io
+from contextlib import redirect_stdout
 
-input_text = sys.stdin.read()
-lexer = LatinoGrammarLexer(InputStream(input_text))
-stream = CommonTokenStream(lexer)
-parser = LatinoGrammarParser(stream)
+app = FastAPI()
 
-tree = parser.source()
+class TextInput(BaseModel):
+    input: str
 
-walker = ParseTreeWalker()
-walker.walk(LatinoToJs(), tree)
+@app.post("/translate")
+async def translate_latino2js(input:TextInput):
+    try:
+        # input_text = open("inputs/05.in", "r").read()
+        # input_text = sys.stdin.read()
+
+        input_text = input.input
+        lexer = LatinoGrammarLexer(InputStream(input_text))
+        stream = CommonTokenStream(lexer)
+        parser = LatinoGrammarParser(stream)
+
+        tree = parser.source()
+
+        walker = ParseTreeWalker()
+        output = io.StringIO()
+        with redirect_stdout(output):
+            walker.walk(LatinoToJs(), tree)
+        print(output.getvalue())
+        return {"result": output.getvalue()}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
